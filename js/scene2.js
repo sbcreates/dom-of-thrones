b6da0855c48aa8998de3f43a34a5ada26a7709f2
sbcreates <drawsarahb@gmail.com>
2017-10-03 16:34:02 -0400

    Wrote code for character one.

--- js/scene2.js
+++ js/scene2.js
@@ -10,6 +10,26 @@ function scene2() {
   // Setup Cast
 
   // Character 1
+  nedContainer = document.createElement('div')
+  stage.append(nedContainer)
+  nedContainer.style.position = 'absolute'
+  nedContainer.style.width = '10%'
+  nedContainer.style.height = '20%'
+  nedContainer.style.top = '50%'
+  nedContainer.style.left = '50%'
+
+  ned = document.querySelector('#ned')
+  nedContainer.append(ned)
+  leather = document.querySelector('#leather-armour')
+  nedContainer.append(leather)
+  ned.style.width = '70%'
+  ned.style.position = 'absolute'
+  ned.style.top = 0
+  leather.style.position = 'absolute'
+  leather.style.bottom = 0
+  nedContainer.style.height = '37%'
+  nedContainer.style.top = '55%'
+  nedContainer.style.left = '25%'
 
   // Character 2
 
