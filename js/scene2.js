51d7f88677706e5cf29e85f4d388b05b2d28eb9d
sbcreates <drawsarahb@gmail.com>
2017-10-03 16:09:31 -0400

    Set up stage for scene 2 of DOM of Thrones

---
+++ js/scene2.js
@@ -0,0 +1,36 @@
+function scene2() {
+
+  // Setup Stage
+  stage = document.querySelector('#stage')
+  stageImg = document.querySelector('#stage-img')
+  castle = document.querySelector('#castle')
+  stageImg.src = castle.src
+
+
+  // Setup Cast
+
+  // Character 1
+
+  // Character 2
+
+  // Setup Props
+
+  // Prop 1
+
+  // Prop 2
+
+  // Action!
+
+  // Stage direction 1
+
+  frame(function() {
+
+  })
+
+  // Stage direction 2
+
+  frame(function() {
+
+  })
+
+}
