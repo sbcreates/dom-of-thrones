a73d10dd7cc01a501460279d5d06092d204be8a0
sbcreates <drawsarahb@gmail.com>
2017-10-03 17:05:07 -0400

    Wrote code for character two and for sword prop.

--- js/scene2.js
+++ js/scene2.js
@@ -22,20 +22,50 @@ function scene2() {
   nedContainer.append(ned)
   leather = document.querySelector('#leather-armour')
   nedContainer.append(leather)
+
   ned.style.width = '70%'
   ned.style.position = 'absolute'
   ned.style.top = 0
   leather.style.position = 'absolute'
   leather.style.bottom = 0
+
   nedContainer.style.height = '37%'
   nedContainer.style.top = '55%'
   nedContainer.style.left = '25%'
 
   // Character 2
+  joffContainer = document.createElement('div')
+  joffContainer.id = 'joff-container'
+  stage.append(joffContainer)
+
+  joffContainer.style.position = 'absolute'
+  joffContainer.style.width = '8%'
+  joffContainer.style.height = '31%'
+  joffContainer.style.top = '55%'
+  joffContainer.style.left = '55%'
+
+  joffrey = document.querySelector('#joffrey')
+  joffContainer.append(joffrey)
+  joffrey.style.width = '75%'
+  joffrey.style.left = '33%'
+  joffrey.style.position = 'absolute'
+  joffrey.style.top = 0
+
+  kingGarb = document.querySelector('#king')
+  joffContainer.append(kingGarb)
+  kingGarb.style.position = 'absolute'
+  kingGarb.style.bottom = 0
 
   // Setup Props
 
   // Prop 1
+  sword = document.querySelector('#sword')
+  stage.append(sword)
+  sword.style.position = 'absolute'
+  sword.style.top = '77%'
+  sword.style.height = '5%'
+  sword.style.left = '31%'
+  sword.style.transform = 'rotateY(150deg)'
 
   // Prop 2
 
